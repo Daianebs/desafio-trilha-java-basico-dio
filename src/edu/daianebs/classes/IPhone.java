10c68e5c0b78a53217d1ea67e6089c4fe222e9aa
DaianeBS <97747870+Daianebs@users.noreply.github.com>
2024-07-18 19:18:58 -0300

    ✨feat: implementando metodos

--- src/edu/daianebs/classes/IPhone.java
+++ src/edu/daianebs/classes/IPhone.java
@@ -7,102 +7,112 @@ import edu.daianebs.interfaces.IMusicPlayer;
 import edu.daianebs.interfaces.IPhoneDevice;
 import edu.daianebs.interfaces.IWebBrowser;
 
-public class IPhone implements IPhoneDevice , IMusicPlayer, IWebBrowser {
+public class IPhone implements IPhoneDevice, IMusicPlayer, IWebBrowser {
   private List<Contact> contacts = new ArrayList<>();
   private List<Music> musics = new ArrayList<>();
   private List<URLFavorite> favorites = new ArrayList<>();
-  
-  @Override
-  public void exibirPagina(String url) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'exibirPagina'");
-  }
-  @Override
-  public void adicionarNovaAba() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'adicionarNovaAba'");
-  }
-  @Override
-  public void atualizarPagina() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'atualizarPagina'");
-  }
-  @Override
-  public void favoritarPagina(String url) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'favoritarPagina'");
-  }
-  @Override
-  public void removerFavorito(String url) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'removerFavorito'");
-  }
-  @Override
-  public List<String> listarFavoritos() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'listarFavoritos'");
-  }
-  @Override
-  public void tocarMusica(String musica) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'tocarMusica'");
-  }
-  @Override
-  public void pausarMusica() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'pausarMusica'");
-  }
-  @Override
-  public void selecionarMusica(String musica) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'selecionarMusica'");
-  }
-  @Override
-  public List<String> listarMusicas() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'listarMusicas'");
-  }
-  @Override
-  public void adicionarMusica(Music musica) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'adicionarMusica'");
-  }
-  @Override
-  public void removerMusica(String nome) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'removerMusica'");
-  }
+
   @Override
   public void ligar(String numero) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'ligar'");
-  }
-  @Override
-  public void atender() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'atender'");
-  }
-  @Override
-  public void iniciarCorreioVoz() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'iniciarCorreioVoz'");
-  }
-  @Override
-  public void adicionarContato(Contact contato) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'adicionarContato'");
-  }
-  @Override
-  public void removerContato(String nome) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'removerContato'");
-  }
-  @Override
-  public List<Contact> listarContatos() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'listarContatos'");
+    System.out.println("Ligando para " + numero);
   }
 
-  
-  
+  @Override
+  public void atender() {
+    System.out.println("Atendendo ligação...");
+  }
+
+  @Override
+  public void iniciarCorreioVoz() {
+    System.out.println("Acessando correio de voz");
+  }
+
+  @Override
+  public void adicionarContato(Contact contato) {
+    contacts.add(contato);
+    System.out.println("Contato adicionado: " + contato.getNome());
+  }
+
+  @Override
+  public void removerContato(String nome) {
+    contacts.removeIf(contact -> contact.getNome().equals(nome));
+    System.out.println("Contato removido: " + nome);
+  }
+
+  @Override
+  public List<Contact> listarContatos() {
+    return new ArrayList<>(contacts);
+  }
+
+  @Override
+  public void tocarMusica(String musica) {
+    System.out.println("Tocando música: " + musica);
+  }
+
+  @Override
+  public void pausarMusica() {
+    System.out.println("Música pausada");
+  }
+
+  @Override
+  public void selecionarMusica(String musica) {
+    System.out.println("Selecionando música: " + musica);
+  }
+
+  @Override
+  public List<String> listarMusicas() {
+    List<String> musicList = new ArrayList<>();
+    for (Music music : musics) {
+      musicList.add(music.getNome());
+    }
+    return musicList;
+  }
+
+  @Override
+  public void adicionarMusica(Music musica) {
+    musics.add(musica);
+    System.out.println("Música adicionada: " + musica.getNome());
+  }
+
+  @Override
+  public void removerMusica(String nome) {
+    musics.removeIf(music -> music.getNome().equals(nome));
+    System.out.println("Música removida: " + nome);
+  }
+
+  @Override
+  public void exibirPagina(String url) {
+    System.out.println("Exibindo página: " + url);
+  }
+
+  @Override
+  public void adicionarNovaAba() {
+    System.out.println("Nova aba adicionada");
+  }
+
+  @Override
+  public void atualizarPagina() {
+    System.out.println("Página atualizada");
+  }
+
+  @Override
+  public void favoritarPagina(String url) {
+    favorites.add(new URLFavorite("Favorito", url));
+    System.out.println("Página favoritada: " + url);
+  }
+
+  @Override
+  public void removerFavorito(String url) {
+    favorites.removeIf(favorite -> favorite.getUrl().equals(url));
+    System.out.println("Favorito removido: " + url);
+  }
+
+  @Override
+  public List<String> listarFavoritos() {
+    List<String> favoriteUrls = new ArrayList<>();
+    for (URLFavorite favorite : favorites) {
+      favoriteUrls.add(favorite.getUrl());
+    }
+    return favoriteUrls;
+  }
 }
