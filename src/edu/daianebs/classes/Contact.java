e12e0929c7715989e687daffa8ee86379660a0e5
DaianeBS <97747870+Daianebs@users.noreply.github.com>
2024-07-18 18:36:34 -0300

    ✨feat: classe contato

--- src/edu/daianebs/classes/Contact.java
+++ src/edu/daianebs/classes/Contact.java
@@ -1,5 +1,23 @@
 package edu.daianebs.classes;
 
 public class Contact {
-  
+  String nome;
+  String numero;
+  public Contact(String nome, String numero) {
+    this.nome = nome;
+    this.numero = numero;
+  }
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public String getNumero() {
+    return numero;
+  }
+  public void setNumero(String numero) {
+    this.numero = numero;
+  }
+
 }
